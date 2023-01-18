31c35c0db1bdd3816aabdb296926ec09900593e1
SamG <16375795+SamG06@users.noreply.github.com>
2023-01-18 08:34:27 -0500

    messing around and sending json using struct

--- backend/server.go
+++ backend/server.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Test struct {
+	Post string `json:"post"`
+	Minutes int `json:"minutes"`
+}
+
+
+func writeHeader(w *http.ResponseWriter){
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Content-Type", "application/json")
+	(*w).WriteHeader(http.StatusCreated)
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		(w).Header().Set("Access-Control-Allow-Origin", "*")
+		writeHeader(&w)
 
-		fmt.Fprint(w, "Uh hi...?")
+		x := Test{Post: "Hello World", Minutes: 9}
+		
+		json.NewEncoder(w).Encode(x)
 	})
 
 	fmt.Print("Starting server at port 3000\n")
