27d452095a8c6cb3933cec7ddabcbbfe7bd51c48
SamG <16375795+SamG06@users.noreply.github.com>
2023-01-19 00:23:46 -0500

    Creating regex routes

--- backend/server.go
+++ backend/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 type Test struct {
@@ -12,25 +13,48 @@ type Test struct {
 	Minutes int `json:"minutes"`
 }
 
-
-func writeHeader(w *http.ResponseWriter){
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Content-Type", "application/json")
-	(*w).WriteHeader(http.StatusCreated)
-}
-
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		writeHeader(&w)
-
-		x := Test{Post: "Hello World", Minutes: 9}
-		
-		json.NewEncoder(w).Encode(x)
-	})
+	http.HandleFunc("/", multiplexer)
 
 	fmt.Print("Starting server at port 3000\n")
 
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func setupForJson(w *http.ResponseWriter){
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Content-Type", "application/json")
+	(*w).WriteHeader(http.StatusCreated)
+}
+
+func multiplexer(w http.ResponseWriter, r *http.Request){
+	switch r.Method {
+	case http.MethodGet:
+		getHandler(w, r)
+	}
+}
+
+var index = regexp.MustCompile(`^/$`)
+
+func getHandler(w http.ResponseWriter, r *http.Request){
+	switch {
+	case index.MatchString(r.URL.Path):
+		returnHomePage(w, r)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404"))
+	}
+}
+
+
+func returnHomePage (w http.ResponseWriter, r *http.Request){
+	setupForJson(&w)
+
+	fmt.Printf("%s", http.MethodGet)
+
+	x := Test{Post: "Hello World", Minutes: 9}
+	
+	json.NewEncoder(w).Encode(x)
 }
