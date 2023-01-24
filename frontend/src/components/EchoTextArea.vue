6cd005166d9abb7ee62fb1cdd32d25a82bc746ce
SamG <16375795+SamG06@users.noreply.github.com>
2023-01-23 19:46:58 -0500

    basic styling setup

---
+++ frontend/src/components/EchoTextArea.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <textarea name="echo-post" placeholder="Create an echo post" id="echo-post"></textarea>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    textarea{
+        resize: none;
+        background-color: bisque;
+        width: 90vw;
+        max-width: 400px;
+        padding: 10px;
+        border-radius: 5px;
+    }
+</style>
