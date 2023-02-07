515a1a2fb697203f35ec761f2b05faf205c5d047
SamG <16375795+SamG06@users.noreply.github.com>
2023-02-06 22:38:01 -0500

    db test

--- backend/server.go
+++ backend/server.go
@@ -9,13 +9,15 @@ import (
 	"regexp"
 )
 
-type Test struct {
-	Post string `json:"post"`
-	Minutes int `json:"minutes"`
+type EchoPost struct {
+	Content string `json:"content"`
+	Date int `json:"date"`
 }
+var db = database.Connection()
 
 func main() {
 	database.SetupDatabase()
+
 	http.HandleFunc("/", multiplexer)
 
 	fmt.Print("Starting server at port 3000\n")
@@ -32,9 +34,12 @@ func setupForJson(w *http.ResponseWriter){
 }
 
 func multiplexer(w http.ResponseWriter, r *http.Request){
+	setupForJson(&w)
 	switch r.Method {
 	case http.MethodGet:
 		getHandler(w, r)
+	case http.MethodPost:
+		postHandler(w, r)
 	}
 }
 
@@ -53,16 +58,79 @@ func getHandler(w http.ResponseWriter, r *http.Request){
 	}
 }
 
+var createPost = regexp.MustCompile(`^/create-post$`)
+
+func postHandler(w http.ResponseWriter, r *http.Request){
+	switch{
+	case createPost.MatchString(r.URL.Path):
+		createEchoPost(w, r)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404"))
+	}
+}
+	
+
+
 
 func returnHomePage (w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("This is the home route of the echo post API"))	
 }
 
 func getEchoPost (w http.ResponseWriter, r *http.Request){
-	setupForJson(&w)
-
-	x := Test{Post: "This is a real Echo Post", Minutes: 8}
+	x := EchoPost{Content: "This is a real Echo Post", Date: 8}
 
 	json.NewEncoder(w).Encode(x)
+}
+
+
+func executeSQL(sql_ string){
+	
+}
+
+func createEchoPost (w http.ResponseWriter, r *http.Request){
+	var post EchoPost
+	
+	err := json.NewDecoder(r.Body).Decode(&post)
+
+	if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+	sqlMakePost := "INSERT INTO EchoPost (content) VALUES (?)"
+
+	tx, err := db.Begin()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer tx.Rollback()
+
+	stmt, err := tx.Prepare(`INSERT INTO echo_post (content) VALUES ($1)`)
+    
+fmt.Print(sqlMakePost)
+
+	if err != nil {
+		log.Fatal(err)
+    }
+    
+	defer stmt.Close()
+
+	_, err = stmt.Exec(post.Content)
+
+	if err != nil {
+		log.Fatal(err)
+    }
+
+	err = tx.Commit()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, "Post: %+v", post)
+	json.NewEncoder(w).Encode(post)
 }
 
